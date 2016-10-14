9110eca37db6f40321f314c5bedaa7a3e3fe5172
Hekla Rún Ámundadóttir <heklaruna@gmail.com>
2016-10-14 12:47:21 +0000

    laga calctest

--- src/CodingKata/test/CalculatorTest.java
+++ src/CodingKata/test/CalculatorTest.java
@@ -7,44 +7,44 @@ public class CalculatorTest {
 	
 	@Test
 	public void testEmptyString() throws Exception {
-		assertEquals(0, StringCalculator.Add(""));
+		assertEquals(0, StringCalculator.add(""));
 	}
 	
 	@Test
 	public void testOneNumber() throws Exception{
-		assertEquals(1, StringCalculator.Add("1"));
+		assertEquals(1, StringCalculator.add("1"));
 	}
 	
 	@Test
 	public void testTwoNumber() throws Exception{
-		assertEquals(3, StringCalculator.Add("1,2"));
+		assertEquals(3, StringCalculator.add("1,2"));
 	}
 	
 	@Test
 	public void testUnknownNumber() throws Exception {
-		assertEquals(6, StringCalculator.Add("1,2,3"));
+		assertEquals(6, StringCalculator.add("1,2,3"));
 	}
 	
 	@Test
 	public void testNewLineDilimitedNumber() throws Exception   {
-		assertEquals(6, StringCalculator.Add("1\n2,3"));
+		assertEquals(6, StringCalculator.add("1\n2,3"));
 	}
 	
 	
 	@Test
 	public void NumGreaterThan1000AreIgnored() throws Exception {
-		assertEquals(15, StringCalculator.Add("10,5,1100"));
+		assertEquals(15, StringCalculator.add("10,5,1100"));
 	}
 	
 	
 	@Test
 	public void DifferentDelimiter() throws Exception {
-		assertEquals(8, StringCalculator.Add("//;\n4;4"));
+		assertEquals(8, StringCalculator.add("//;\n4;4"));
 	}
 	@Test(expected=IllegalArgumentException.class)
 	public void negativeNumberWillThrowException() throws Exception{
-		StringCalculator.Add("-1");
-		StringCalculator.Add("2, -6, 5, -4");
+		StringCalculator.add("-1");
+		StringCalculator.add("2, -6, 5, -4");
 	}
 	
 }	
